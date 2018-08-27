d23f6d29bbfa132d4af5d954f98bd911f5f37d8f
vidy <zhengquan.li@remarkdip.com>
2018-08-27 13:54:24 +0800

    拼接图片域名

--- Serializer/FileEndpointNormalizer.php
+++ Serializer/FileEndpointNormalizer.php
@@ -7,6 +7,8 @@ use Symfony\Component\Serializer\SerializerAwareInterface;
 use Symfony\Component\Serializer\SerializerAwareTrait;
 use App\Bundle\FileBundle\Metadata\MetadataReader;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 /**
  * 给链接添加域名
@@ -16,6 +18,12 @@ class FileEndpointNormalizer implements NormalizerInterface, SerializerAwareInte
     private $metadataReader;
     private $serializer;
     private $assetEndpoint;
+    private $called = [];
+
+    /**
+     * @var PropertyAccessor
+     */
+    private static $propertyAccessor;
 
     public function __construct(
         MetadataReader $metadataReader,
@@ -35,14 +43,16 @@ class FileEndpointNormalizer implements NormalizerInterface, SerializerAwareInte
 
     protected function addEndpoint($object)
     {
+        $this->called[spl_object_hash($object)] = true;
+
         $data = [];
 
         $fields = $this->metadataReader->getLinks(get_class($object));
+
         foreach ($fields as $name => $field) {
             $property = strtolower(preg_replace('/[A-Z]/', '_\\0', lcfirst($name)));
-
             if ($field['absolute']) {
-                $data[$property] = $this->assetEndpoint. $object->{'get'.ucfirst($name)};
+                $data[$property] = $this->assetEndpoint. $this->getPropertyAccessor()->getValue($object, $name);
             }
         }
 
@@ -56,6 +66,26 @@ class FileEndpointNormalizer implements NormalizerInterface, SerializerAwareInte
 
     public function supportsNormalization($object, $format = null)
     {
-        return is_object($object) && $this->metadataReader->hasFile(get_class($object));
+        if (!is_object($object)) {
+            return false;
+        }
+
+        if (array_key_exists(spl_object_hash($object), $this->called)) {
+            return false;
+        }
+
+        return $this->metadataReader->hasFile(get_class($object));
+    }
+
+     /**
+     * @return PropertyAccessor
+     */
+    private static function getPropertyAccessor()
+    {
+        if (!self::$propertyAccessor) {
+            self::$propertyAccessor = PropertyAccess::createPropertyAccessor();
+        }
+
+        return self::$propertyAccessor;
     }
 }
