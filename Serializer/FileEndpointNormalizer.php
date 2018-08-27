14d4b8514a2cdadacceb575f55fdecdfe2454438
vidy <zhengquan.li@remarkdip.com>
2018-08-27 13:59:58 +0800

    不返回图片地址

--- Serializer/FileEndpointNormalizer.php
+++ Serializer/FileEndpointNormalizer.php
@@ -52,7 +52,9 @@ class FileEndpointNormalizer implements NormalizerInterface, SerializerAwareInte
         foreach ($fields as $name => $field) {
             $property = strtolower(preg_replace('/[A-Z]/', '_\\0', lcfirst($name)));
             if ($field['absolute']) {
-                $data[$property] = $this->assetEndpoint. $this->getPropertyAccessor()->getValue($object, $name);
+                // $data[$property] = $this->assetEndpoint. $this->getPropertyAccessor()->getValue($object, $name);
+                //@todo: remove following code
+                $data[$property] = '';
             }
         }
 
